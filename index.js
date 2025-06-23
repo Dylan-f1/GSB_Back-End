b4a97d819cda189b3997a909e0f8180fb0460f2e
Dylan-f1 <dylan.fernandes.travail@gmail.com>
2025-06-23 14:43:23 +0200

    commit à la vite fait

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const userRoute = require('./routes/user_route')
 const authenticationRoute = require('./routes/authentification_route')
+const reminderRoute = require('./routes/reminder_routes');
 const billRoute = require('./routes/bill_route')
 const startReminderJob = require('./cron/remindercron.js');
+
 
 mongoose.connect('mongodb+srv://Dylan:Go14@cluster0.g7q9apz.mongodb.net/GSB-Back-End')
 const db = mongoose.connection;
@@ -20,6 +22,7 @@ app.use(cors())
 app.use('/users', userRoute)
 app.use('/auth', authenticationRoute)
 app.use('/bills', billRoute)
+app.use('/reminder', reminderRoutes);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
